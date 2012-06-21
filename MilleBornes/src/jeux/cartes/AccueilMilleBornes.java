f085fe10dc48a069d05868776137b17ccc39e531
loic.martinux@gmail.com <loic.martinux@gmail.com@f7d90370-fd5c-672f-111c-14806915b62b>
2012-06-21 16:25:20 +0000

    Ajout du main et de la classe qui va s'occuper de l'interface graphique d'accueil.

---
+++ MilleBornes/src/jeux/cartes/AccueilMilleBornes.java
@@ -0,0 +1,15 @@
+/**
+ * AccueilMilleBornes.java    21/06/2012
+ */
+
+package jeux.cartes;
+
+/**
+ * Fenêtre d'accueil du jeu de 1000 bornes
+ * @author Loïc Martinez
+ * @version 1.0
+ */
+
+public class AccueilMilleBornes {
+
+}
