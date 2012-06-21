4149420fe87e43e6cda87f78f6231ceca9568d07
loic.martinux@gmail.com <loic.martinux@gmail.com@f7d90370-fd5c-672f-111c-14806915b62b>
2012-06-21 22:55:59 +0000

    Bug-fix sur le quit quand on essaye de créer le serveur et que quelqu'un d'autre l'a déjà fait.

--- trunk/MilleBornes/src/jeux/cartes/serveur/ServeurRequetes.java
+++ trunk/MilleBornes/src/jeux/cartes/serveur/ServeurRequetes.java
@@ -23,84 +23,85 @@ import jeux.cartes.rmi.RequetesReseau;
 public class ServeurRequetes extends UnicastRemoteObject implements
 RequetesReseau {
 
-    /** Nom du serveur RMI */
-    private String name;
+	/** Nom du serveur RMI */
+	private String name;
 
-    /** Permet de savoir si le registre a été créé ou non */
-    private boolean registryCree = false;
+	/** Permet de savoir si le registre a été créé ou non */
+	private boolean registryCree = false;
 
-    /**
-     * Constructeur
-     * @param name, le nom du serveur RMI
-     * @throws RemoteException, si un problème survient via RMI
-     */
-    public ServeurRequetes(String name) throws RemoteException {
-        this.name = name;
-    }
-    
-    /** Logger */
-    private static Logger logger = Logger.getLogger(ServeurRequetes.class);
+	/**
+	 * Constructeur
+	 * @param name, le nom du serveur RMI
+	 * @throws RemoteException, si un problème survient via RMI
+	 */
+	public ServeurRequetes(String name) throws RemoteException {
+		this.name = name;
+	}
 
-    /**
-     * Constructeur
-     * @param name, le nom du serveur RMI
-     * @param port, le port du createRegistry
-     * @throws RemoteException, si un problème survient via RMI
-     */
-    public ServeurRequetes(String name, int port) throws RemoteException {
-        this.name = name;
+	/** Logger */
+	private static Logger logger = Logger.getLogger(ServeurRequetes.class);
 
-        // On réserve le port
-        LocateRegistry.createRegistry(port);
+	/**
+	 * Constructeur
+	 * @param name, le nom du serveur RMI
+	 * @param port, le port du createRegistry
+	 * @throws RemoteException, si un problème survient via RMI
+	 */
+	public ServeurRequetes(String name, int port) throws RemoteException {
+		this.name = name;
 
-        lancer();
-        registryCree = true;
-    }
+		// On réserve le port
+		LocateRegistry.createRegistry(port);
 
-    /**
-     * Permet de lancer le serveur
-     */
-    public void lancer() {
-        try {
-            Naming.rebind("rmi://" + InetAddress.getLocalHost().getHostAddress() + "/" + name, this);
-        } catch (MalformedURLException e) {
-            logger.error("ServeurRequetes.lancer() : MalformedURLException");
-        } catch (RemoteException e) {
-            logger.error("ServeurRequetes.lancer() : RemoteException");
-        } catch (UnknownHostException ex) {
-            logger.error("ServeurRequetes.lancer() : UnknownHostException");
-        }
-    }
+		lancer();
+		registryCree = true;
+	}
 
-    /**
-     * Permet d'arrêter le serveur
-     */
-    public void arreter() {
-        try {
-            Naming.unbind(name);
-            UnicastRemoteObject.unexportObject(this, true);
-            // System.gc();
-        } catch (RemoteException e) {
-            logger.error("ServeurRequetes.arreter() : RemoteException");
-        } catch (MalformedURLException e) {
-            logger.error("ServeurRequetes.arreter() : MalformedURLException");
-        } catch (NotBoundException e) {
-            logger.error("ServeurRequetes.arreter() : NotBoundException");
-        }
-    }
+	/**
+	 * Permet de lancer le serveur
+	 */
+	public void lancer() {
+		try {
+			Naming.rebind("rmi://" + InetAddress.getLocalHost().getHostAddress() + "/" + name, this);
+		} catch (MalformedURLException e) {
+			logger.error("ServeurRequetes.lancer() : MalformedURLException");
+		} catch (RemoteException e) {
+			logger.error("ServeurRequetes.lancer() : RemoteException");
+		} catch (UnknownHostException ex) {
+			logger.error("ServeurRequetes.lancer() : UnknownHostException");
+		}
+	}
 
-    /**
-     * Setter de registryCree
-     * @param b, true ou false
-     */
-    public void setRegistryCree(boolean b) {
-        registryCree = b;
-    }
+	/**
+	 * Permet d'arrêter le serveur
+	 */
+	public void arreter() {
+		try {
+			if (Naming.list(name).length > 0) {
+				Naming.unbind(name);
+				UnicastRemoteObject.unexportObject(this, true);
+			}
+		} catch (RemoteException e) {
+			logger.error("ServeurRequetes.arreter() : RemoteException");
+		} catch (MalformedURLException e) {
+			logger.error("ServeurRequetes.arreter() : MalformedURLException");
+		} catch (NotBoundException e) {
+			logger.error("ServeurRequetes.arreter() : NotBoundException");
+		}
+	}
 
-    /**
-     * @return registryCree
-     */
-    public boolean getRegistryCree() {
-        return registryCree;
-    }
+	/**
+	 * Setter de registryCree
+	 * @param b, true ou false
+	 */
+	public void setRegistryCree(boolean b) {
+		registryCree = b;
+	}
+
+	/**
+	 * @return registryCree
+	 */
+	public boolean getRegistryCree() {
+		return registryCree;
+	}
 }
