d0b9d1b14313acd7e34d504029846ce37584b38c
loic.martinux@gmail.com <loic.martinux@gmail.com@f7d90370-fd5c-672f-111c-14806915b62b>
2012-06-21 16:43:12 +0000

    Move dans trunk.

---
+++ trunk/MilleBornes/src/jeux/cartes/RequetesClient.java
@@ -0,0 +1,57 @@
+/**
+ * RequetesClient.java    19/04/2012
+ */
+
+package jeux.cartes;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import jeux.cartes.rmi.RequetesReseau;
+
+import org.apache.log4j.Logger;
+
+import jeux.utilitaires.Configuration;
+
+/**
+ * Permet la passerelle Client - Serveur via RMI
+ * @author Loïc Martinez & Aboulkacem Hamid
+ */
+public class RequetesClient {
+
+    /** Connexion avec le ServeurRequetes */
+    private static RequetesReseau connexionServer = null;
+
+    /** Configuration du client */
+    private Configuration conf = new Configuration("configClient.properties");
+
+    /** Logger */
+    private static Logger logger = Logger.getLogger(RequetesClient.class);
+    
+    /**
+     * Constructeur par défaut
+     * @throws MalformedURLException, si problème durant le lookup
+     * @throws RemoteException, si problème via RMI
+     */
+    public RequetesClient() throws NotBoundException, RemoteException {
+        try {
+
+            connexionServer = (RequetesReseau) Naming.lookup("rmi://"
+                    + conf.valueOf("address") + "/serveur");
+
+        } catch (MalformedURLException ex) {
+            new JDError("Mauvaise URL");
+            logger.error("RequetesClient.RequetesClient() : MalformedURLException");
+        }
+    }
+
+    /**
+     * @return connexionServer
+     */
+    public RequetesReseau getReseau() {
+        return connexionServer;
+    }
+}
---
+++ trunk/MilleBornes/src/jeux/cartes/rmi/RequetesReseau.java
@@ -0,0 +1,16 @@
+/**
+ * requetesReseau.java    19/04/2012
+ */
+
+package jeux.cartes.rmi;
+
+import java.rmi.Remote;
+
+/**
+ * Méthode à écrire dans ServeurRequetes (to override)
+ * @author Aboulkacem Hamid & Loïc Martinez
+ */
+
+public interface RequetesReseau extends Remote {
+
+}
