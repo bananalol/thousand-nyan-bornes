b97c43947a6661c89e1c67abed36873dfcbb1ffa
loic.martinux@gmail.com <loic.martinux@gmail.com@f7d90370-fd5c-672f-111c-14806915b62b>
2012-06-21 20:38:08 +0000

    Ajout du nombre de chaque carte dans le deck.

---
+++ trunk/MilleBornes/src/jeux/cartes/carte/Deck.java
@@ -0,0 +1,43 @@
+/**
+ * Deck.java    21/06/2012
+ */
+
+package jeux.cartes.carte;
+
+/**
+ * Le paquet de cartes
+ * @author Loïc Martinez
+ * @version 1.0
+ */
+
+public class Deck {
+
+	/**
+	 * Distances
+	 * 10x 25km
+	 * 10x 50km
+	 * 10x 75km
+	 * 12x 100km
+	 * 4x 200km
+	 * 
+	 * Bottes
+	 * 1x As du volant
+	 * 1x Camion-citerne
+	 * 1x Increvable
+	 * 1x Prioritaire
+	 * 
+	 * Attaques
+	 * 3x Accident
+	 * 3x Panne d'essence
+	 * 3x Crevaison
+	 * 4x Limite de vitesse
+	 * 5x Feu rouge
+	 * 
+	 * Parades
+	 * 6x Réparations
+	 * 6x Essence
+	 * 6x Roue de secours
+	 * 6x Fin de limite
+	 * 14x Feu vert
+	 */
+}
